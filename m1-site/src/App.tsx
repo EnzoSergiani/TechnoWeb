3e55bd0ba3c17e3527636e217b1fa6ce7a98d930
Enzo Sergiani <enzo.sergiani@protonmail.com>
2024-11-02 15:44:30 +0100

    init

--- m1-site/src/App.tsx
+++ m1-site/src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import BooksIndex from "./pages/books/books_index";
+import BookDetail from "./pages/books/books_details";
+import AuthorsIndex from "./pages/authors/authors_index";
+import AuthorDetails from "./pages/authors/authors_details";
 
 function App() {
   return (
@@ -8,7 +11,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/books" element={<BooksIndex />} />
+          <Route path="books" element={<BooksIndex />} />
+          <Route path="books/:bookId" element={<BookDetail />} />
+          <Route path="author" element={<AuthorsIndex />} />
+          <Route path="author/:authorId" element={<AuthorDetails />} />
           {/* <Route path="*" element={<Error />} /> */}
         </Routes>
       </BrowserRouter>
