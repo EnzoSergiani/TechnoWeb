bf16c4201631f57280ada3d0a989bb8f5f062827
Enzo Sergiani <enzo.sergiani@protonmail.com>
2024-11-08 18:57:26 +0100

    online folder

---
+++ m1-site/src/online/book/book.ts
@@ -0,0 +1,43 @@
+import { BookInterface } from "@/export/interface";
+import axiosApi from "@/online/axios/axiosApi";
+
+export const fetchAllBooks = async () => {
+    try {
+        const response = await axiosApi.get("/books");
+        //console.log(response);
+        return response.data;
+    }
+    catch(error){
+        console.error("Error fetching all books:", error);
+    }
+};
+
+export const fetchBookById = async (id: number) => {
+    try {
+      const response = await axiosApi.get(`/books/${id}`);
+      //console.log(response);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching one book:", error);
+    }
+};
+
+export const createBook = async (bookData: BookInterface) => {
+    try {
+      const response = await axiosApi.post("/books", bookData);
+      //console.log("Book created:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error creating book:", error);
+    }
+};
+
+export const deleteBookById = async (id: number) => {
+    try {
+      const response = await axiosApi.delete(`/books/${id}`);
+      //console.log("Book deleted:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error deleting book:", error);
+    }
+};
