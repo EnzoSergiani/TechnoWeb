ef52351fc88df375fd3cc8e36a1ef17cfebc4378
Enzo Sergiani <enzo.sergiani@protonmail.com>
2024-11-15 21:35:40 +0100

    comment drawer update

--- m1-site/src/components/commentDrawer.tsx
+++ m1-site/src/components/commentDrawer.tsx
@@ -9,19 +9,47 @@ interface CommentDrawerProps {
 
 const CommentDrawer: React.FC<CommentDrawerProps> = ({ isOpen, onClose }) => {
   const [drawerOpen, setDrawerOpen] = useState(isOpen)
+  const [comment, setComment] = useState('')
+  const [comments, setComments] = useState<string[]>([])
 
   useEffect(() => {
     setDrawerOpen(isOpen)
   }, [isOpen])
 
+  const handleSendComment = () => {
+    setComments([...comments, comment])
+    setComment('')
+  }
+
   return (
     <div>
       <Drawer anchor="left" open={drawerOpen} onClose={onClose}>
-        <div className="w-96 pl-10 pr-10 pt-5">
+        <div className="h-full w-96 p-10">
           <span className="text-lg font-bold">Commentaires :</span>
+
           <div className="mt-4">
-            <input type="text" placeholder="Enter your comment" className="w-full rounded border border-gray-300 p-2" />
-            <Button className="mt-2">Send</Button>
+            {comments
+              .slice(0)
+              .reverse()
+              .map((comment, index) => (
+                <div key={index} className="border-b border-gray-200 py-2">
+                  <p>{comment}</p>
+                </div>
+              ))}
+          </div>
+
+          <div className="sticky bottom-0 bg-white p-4">
+            <div className="w-full rounded border border-black bg-white p-2">
+              <textarea
+                placeholder="Enter your comment"
+                className="h-32 w-full p-2"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              />
+              <Button className="mt-2" onClick={handleSendComment}>
+                Send
+              </Button>
+            </div>
           </div>
         </div>
       </Drawer>
